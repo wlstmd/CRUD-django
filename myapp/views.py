1a7c1408be73ce4a77d5def7a18838be2c995ed3
jinseung <jinseung0327@gmail.com>
2024-01-15 19:45:56 +0900

    FEAT: read

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+topics = [
+  {'id':1, 'title':'routing', 'body':'Routing is ..'},
+  {'id':2, 'title':'view', 'body':'View is ..'},
+  {'id':3, 'title':'Model', 'body':'Model is ..'},
+]
+
+def HTMLTemplate(articleTag):
+  global topics
+  ol = ''
+  for topic in topics:
+    ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+  return f'''
+  <html>
+  <body>
+    <h1><a href="/">Django</a></h1>
+    <ul>
+      {ol}
+    </ul>
+    {articleTag}
+  </body>
+  </html>
+  '''
+
+def index(request):
+  article = '''
+  <h2>Welcome</h2>
+  Hello, Django
+  '''
+  return HttpResponse(HTMLTemplate(article))
+
+def read(request, id):
+  global topics
+  article = ''
+  for topic in topics:
+    if topic['id'] == int(id):
+      article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+  return HttpResponse(HTMLTemplate(article))
+
+def create(request):
+  return HttpResponse('Create')
